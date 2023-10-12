2995cd41c845e0c622b072613835bc58af736336
Constantin Clerc <102235607+c22dev@users.noreply.github.com>
2023-10-12 13:36:08 +0200

    WhatsApp

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://github.com/PlayCover/PlayCover/releases/download/3.0.0-beta.1/PlayCover_3.0.0-beta.1.dmg"
+dmg_url="https://web.whatsapp.com/desktop/mac_native/release/?configuration=Release"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
