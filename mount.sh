fe827ea0228699dbbd41aaf2e199de0aea7a0cfa
Constantin Clerc <102235607+c22dev@users.noreply.github.com>
2023-10-14 00:34:52 +0200

    Geometry Dash 2.11.3

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://github.com/PlayCover/PlayCover/releases/download/3.0.0-beta.1/PlayCover_3.0.0-beta.1.dmg"
+dmg_url="https://d241.userscloud.net/d/hul5tqustn2fvxijvhmzfecaknvnllquga2egd4blrw6snraqu3fzwm4sfxjdk2yndsdm4q5/Geometry_dash_2_113.dmg"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
