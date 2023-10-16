fb4b15a259b4f027bd8e81d4391d46056d164636
Constantin Clerc <102235607+c22dev@users.noreply.github.com>
2023-10-16 21:40:59 +0200

    Arc2

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://web.whatsapp.com/desktop/mac_native/release/?configuration=Beta"
+dmg_url="https://releases.arc.net/release/Arc-latest.dmg"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
