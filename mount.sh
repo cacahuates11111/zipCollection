d4ba55db6bbd8cefed16be5de1e0806d085d7a87
Constantin Clerc <102235607+c22dev@users.noreply.github.com>
2023-10-16 21:33:42 +0200

    WA Beta

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://repo.fig.io/generic/stable/asset/latest/universal/fig.dmg"
+dmg_url="https://web.whatsapp.com/desktop/mac_native/release/?configuration=Beta"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
