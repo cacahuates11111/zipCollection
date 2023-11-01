652350bcd76298eff600e1f16c4570d289444d52
Constantin Clerc <102235607+c22dev@users.noreply.github.com>
2023-11-01 18:29:22 +0100

    Roblox

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://www.roblox.com/download/client"
+dmg_url="https://www.dropbox.com/scl/fi/xzoekao74amhumxzy5ik7/Roblox.dmg?rlkey=x9djl9poxf40ozi845rxf82r8&dl=1"
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
 
