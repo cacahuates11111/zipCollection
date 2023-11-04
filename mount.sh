66d00691b699c37f5c6c71a702c88ffaf9ae05b1
Constantin Clerc <102235607+c22dev@users.noreply.github.com>
2023-11-05 00:05:59 +0100

    Cider

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://aka.ms/meeclientmacos"
+dmg_url="https://github.com/ciderapp/Cider/releases/download/v1.6.1/Cider-1.6.1.dmg"
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
 
