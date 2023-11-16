75543e5c351cd739ade09485582dac10f1b06e23
Constantin Clerc <102235607+c22dev@users.noreply.github.com>
2023-11-16 09:12:36 +0100

    Docker

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://github.com/transmission/transmission/releases/download/4.0.4/Transmission-4.0.4.dmg"
+dmg_url="https://desktop.docker.com/mac/main/arm64/Docker.dmg?utm_source=docker&utm_medium=webreferral&utm_campaign=dd-smartbutton&utm_location=module&_gl=1*13mtx4i*_ga*MTk4NzU2MzIyMS4xNzAwMTIyMjQy*_ga_XJWPQMJYHQ*MTcwMDEyMjI0Mi4xLjEuMTcwMDEyMjI0Ny41NS4wLjA."
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
 
